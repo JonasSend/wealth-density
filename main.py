2698c9c8b718c294da57a5562cc8fbee1d596f13
JonasSend <jonas.send@t-online.de>
2023-02-03 19:46:53 +0100

    change to night-colours, fill no data with median

--- main.py
+++ main.py
@@ -56,16 +56,18 @@ for i in range(len(old_names)):
 def save_plot_map_array(_map_array: np.ndarray, image_name: str) -> None:
     map_values = np.concatenate(_map_array[0], axis=0)
     map_values = map_values[map_values > 0]
+    # boundaries arbitrary - chosen for nice visual apearance
     bounds = [-200, -1] + list(np.percentile(map_values, [25, 43, 57, 69, 79, 87, 93, 97, 99]))
 
-    our_cmap = cm.get_cmap("hot_r", 10)
+    # our_cmap = cm.get_cmap("hot_r", 10)
+    our_cmap = cm.get_cmap("afmhot", 10)
     newcolors = our_cmap(np.linspace(0, 1, 10))
     background_colour = np.array([0, 0, 0, 0])
     newcolors = np.vstack((background_colour, newcolors))
     our_cmap = ListedColormap(newcolors)
     norm = colors.BoundaryNorm(bounds, our_cmap.N)
 
-    fig, ax = plt.subplots(facecolor="#D5E9E8")
+    fig, ax = plt.subplots(facecolor="#000000")
     ax.imshow(_map_array[0], norm=norm, cmap=our_cmap)
     ax.axis("off")
     plt.savefig(image_name + ".png", dpi=1000, bbox_inches="tight", pad_inches=0)
@@ -75,75 +77,98 @@ map_array = population_density_raster.read()
 save_plot_map_array(map_array, "population_density")
 
 
-# for wealth data, multiply population data by wealth per capita if available
+# for wealth data, multiply population data by wealth per capita
 # this gives a simple estimate of wealth/area in USD/km^2
-# no data is treated as no wealth
+# replace no data with median
 
 # create "blank canvas" for wealth density
 map_array[0][map_array[0] > 0.0] = 0.0
+median_wealth = wealth_df["mean_wealth"].median()
 
-for country in wealth_df["country"]:
+for country in shape_df["ADMIN"]:
     shape = [mapping(shape_df["geometry"][shape_df["ADMIN"] == country].iloc[0])]
     country_array, out_transform = mask(population_density_raster, shape, nodata=0)
 
-    country_array *= wealth_df.loc[wealth_df["country"] == country, "mean_wealth"].iloc[0]
+    country_wealth = wealth_df.loc[wealth_df["country"] == country, "mean_wealth"]
+    if len(country_wealth) == 0:
+        country_wealth = median_wealth
+    else:
+        country_wealth = country_wealth.iloc[0]
+    country_array *= country_wealth
     map_array += country_array
     print(country)
 
 save_plot_map_array(map_array, "wealth_density")
 
 
-### load both images and generate GIF that transitions between them ###
+### load both images and generate loop GIF that transitions between them ###
 
-font = ImageFont.truetype("consola.ttf", 300)
+font = ImageFont.truetype("tahoma.ttf", 150)
+font = ImageFont.truetype("segoeui.ttf", 150)
+# font colour: median colour of the plot's colour palette
+font_colour = (255, 156, 28)
+text_position = (250, 1900)
+
+# it makes sense to manually adjust a few colour "anomalies" in the population density map before loading
+# a bit hacky: create the image with and without text to let text fade in and out
 
 population_image = Image.open('population_density.png')
-image_draw = ImageDraw.Draw(population_image)
-image_draw.text((1600, 1800), "population", font=font, fill=(0, 0, 0))
+population_image_with_text = population_image.copy()
+image_draw = ImageDraw.Draw(population_image_with_text)
+image_draw.text(text_position, "Population", font=font, fill=font_colour)
 population_image_array = np.array(population_image.convert('RGB'))
+population_image_with_text_array = np.array(population_image_with_text.convert('RGB'))
 
 wealth_image = Image.open('wealth_density.png')
-image_draw = ImageDraw.Draw(wealth_image)
-image_draw.text((1850, 1800), "wealth", font=font, fill=(0, 0, 0))
+wealth_image_with_text = wealth_image.copy()
+image_draw = ImageDraw.Draw(wealth_image_with_text)
+image_draw.text(text_position, "Wealth", font=font, fill=font_colour)
 wealth_image_array = np.array(wealth_image.convert('RGB'))
+wealth_image_with_text_array = np.array(wealth_image_with_text.convert('RGB'))
 
 image_shape = population_image_array.shape
 
 # transform into flat arrays to facilitate easier processing
 population_image_flat_array = population_image_array.reshape((-1,3))
+population_image_with_text_flat_array = population_image_with_text_array.reshape((-1,3))
 wealth_image_flat_array = wealth_image_array.reshape((-1,3))
+wealth_image_with_text_flat_array = wealth_image_with_text_array.reshape((-1,3))
 
+# set up images for animation
 fig = plt.figure(dpi=600, frameon=False)
 ax = plt.axes()
 ax.axis("off")
 empty_image = ax.imshow(np.zeros(image_shape))
-fps = 25
+fps = 5
+# show each image for 1.5 seconds and use 1 second for transition between the two
 frames = fps * 5
+short_segment = round(frames * 1.5 / 5)
+long_segment = round(frames / 5)
 
 
 # use empty image and fill with content for each frame
+# hacky if-else clause to determine which images are used for fade-ins and -outs
 def animate(i):
-    weight_wealth = calculate_weight(i)
-    empty_image.set_array(fade(population_image_flat_array, wealth_image_flat_array, weight_wealth).reshape(image_shape))
+    if i <= long_segment:
+        empty_image.set_array(population_image_with_text_flat_array.reshape(image_shape))
+    elif i <= round(short_segment/2) + long_segment:
+        empty_image.set_array(fade(population_image_with_text_flat_array, 1 - (2 * (i - long_segment) / short_segment)).reshape(image_shape))
+    elif i <= short_segment + long_segment:
+        empty_image.set_array(fade(wealth_image_with_text_flat_array, (2 * (i - long_segment) / short_segment) - 1).reshape(image_shape))
+    elif i <= long_segment * 2 + short_segment:
+        empty_image.set_array(wealth_image_with_text_flat_array.reshape(image_shape))
+    elif i <= round(short_segment/2) + long_segment * 2 + short_segment:
+        empty_image.set_array(fade(wealth_image_with_text_flat_array, 1 - (2 * (i - (long_segment * 2 + short_segment)) / short_segment)).reshape(image_shape))
+    else:
+        empty_image.set_array(fade(population_image_with_text_flat_array, (2 * (i - (long_segment * 2 + short_segment)) / short_segment) - 1).reshape(image_shape))
     return [empty_image]
 
 
-def fade(flat_image_1, flat_image_2, weight_image_2):
-    faded_image = (flat_image_1 * (1 - weight_image_2)) + (flat_image_2 * weight_image_2)
+# mix image with text with a 50/50 mix of population and wealth images without text
+def fade(image_flat_array, weight_image):
+    weight_other_images = (1 - weight_image)
+    faded_image = (image_flat_array * weight_image) + ((population_image_flat_array + wealth_image_flat_array) * weight_other_images)
     return faded_image.astype(int)
-
-
-def calculate_weight(i):
-    short_segment = round(frames * 1.5 / 5)
-    long_segment = round(frames / 5)
-    if i <= long_segment:
-        return 0
-    elif i <= short_segment + long_segment:
-        return (i - long_segment) / short_segment
-    elif i <= long_segment * 2 + short_segment:
-        return 1
-    else:
-        return 1 - (i - (long_segment * 2 + short_segment)) / short_segment
 
 
 population_wealth_animation = animation.FuncAnimation(fig, animate, frames=frames)
