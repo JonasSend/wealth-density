1a88fd7a9ead496f1f5b1bd0e20adf982134f5c4
jonassend <jonas@jonassend.com>
2023-08-13 20:27:57 +0200

    change font, signature, and text transitions and refactor

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from matplotlib.image import AxesImage
 from shapely.geometry import mapping
 from PIL import Image, ImageDraw, ImageFont
 from PIL.PngImagePlugin import PngImageFile
+from typing import List
 
 
 """ download and process data """
@@ -56,17 +57,18 @@ shape_df = gpd.read_file(data_path + "/ne_10m_admin_0_countries.shp")
 
 """ save raster data as images """
 
+colours = colormaps["afmhot"](np.linspace(0, 1, 10))
+background_colour = np.array([0, 0, 0, 0])
+colours = np.vstack((background_colour, colours))
+
 
 def save_as_map(_map_array: np.ndarray, image_name: str) -> None:
     map_values = np.concatenate(_map_array[0], axis=0)
     map_values = map_values[map_values > 0]
-    # boundaries arbitrary - chosen for nice visual apearance
+    # boundaries relatively arbitrary - chosen for nice visual apearance
     bounds = [-200, -1] + list(np.percentile(map_values, [25, 43, 57, 69, 79, 87, 93, 97, 99]))
-
-    newcolors = colormaps["afmhot"](np.linspace(0, 1, 10))
-    background_colour = np.array([0, 0, 0, 0])
-    newcolors = np.vstack((background_colour, newcolors))
-    colour_map = ListedColormap(newcolors)
+    
+    colour_map = ListedColormap(colours)
     norm = colors.BoundaryNorm(bounds, colour_map.N)
 
     _fig, _ax = plt.subplots(facecolor="#000000")
@@ -104,81 +106,106 @@ save_as_map(map_array, "wealth_density")
 
 """ load both images and generate loop GIF that transitions between them """
 
-font = ImageFont.truetype("Tahoma.ttf", 150)
-font_signature = ImageFont.truetype("Tahoma.ttf", 50)
-# font colour: median colour of the plot's colour palette
-font_colour = (255, 156, 28)
-text_position = (250, 1900)
-text_position_signature = (4200, 2006) 
+font = ImageFont.truetype("Avenir Next.ttc", 170, index=10)
+font_signature = ImageFont.truetype("Avenir Next.ttc", 70, index=10)
+font_colour_index = 8 # pick colour from the map's colour palette - determines length of transition
+font_colour_signature = (255, 255, 255)
+text_position = (2325, 1900)
+text_position_signature = (4060, 2120) 
 
 
 def add_signature(_image_draw: ImageDraw.ImageDraw) -> None:
-    _image_draw.text(text_position_signature, "Jonas Send", font=font_signature, fill=font_colour)
+    _image_draw.text(text_position_signature, "jonassend.com", font=font_signature, fill=font_colour_signature)
     
     
 def convert_to_rgb_array(_image: PngImageFile) -> np.ndarray:
     return np.array(_image.convert('RGB'))
 
 
-# create the images with and without text to let text fade in and out
+def mix(image1: np.ndarray, image2: np.ndarray, weight1: float) -> np.ndarray:
+    mixed_image = image1 * weight1 + image2 * (1- weight1)
+    return mixed_image.astype(int)
+
+
+# create the images with different font colours
 population_image = Image.open('population_density.png')
 image_draw = ImageDraw.Draw(population_image)
 add_signature(image_draw)
-population_image_array = convert_to_rgb_array(population_image)
 
-population_image_with_text = population_image.copy()
-image_draw = ImageDraw.Draw(population_image_with_text)
-image_draw.text(text_position, "Population", font=font, fill=font_colour)
-add_signature(image_draw)
-population_image_with_text_array = convert_to_rgb_array(population_image_with_text)
+image_shape = convert_to_rgb_array(population_image).shape
 
 wealth_image = Image.open('wealth_density.png')
 image_draw = ImageDraw.Draw(wealth_image)
 add_signature(image_draw)
-wealth_image_array = convert_to_rgb_array(wealth_image)
 
-wealth_image_with_text = wealth_image.copy()
-image_draw = ImageDraw.Draw(wealth_image_with_text)
-image_draw.text(text_position, "Wealth", font=font, fill=font_colour)
-add_signature(image_draw)
-wealth_image_with_text_array = convert_to_rgb_array(wealth_image_with_text)
+population_image_array = convert_to_rgb_array(population_image).reshape((-1, 3))
+wealth_image_array = convert_to_rgb_array(wealth_image).reshape((-1, 3))
 
-image_shape = population_image_array.shape
 
-# transform into flat arrays to facilitate easier processing
-population_image_flat_array = population_image_array.reshape((-1, 3))
-population_image_with_text_flat_array = population_image_with_text_array.reshape((-1, 3))
-wealth_image_flat_array = wealth_image_array.reshape((-1, 3))
-wealth_image_with_text_flat_array = wealth_image_with_text_array.reshape((-1, 3))
+def create_transition_images(image1: PngImageFile, image2: PngImageFile, text: str, ) -> List[np.ndarray]:
+    _images = []
+    
+    for i in range(1, font_colour_index + 1):
+        font_colour = tuple(int(x * 255) for x in colours[i][:-1])
+        
+        image1_with_text = image1.copy()
+        image_draw = ImageDraw.Draw(image1_with_text)
+        image_draw.text(text_position, text, font=font, fill=font_colour, anchor="mm")
+        
+        image2_with_text = image2.copy()
+        image_draw = ImageDraw.Draw(image2_with_text)
+        image_draw.text(text_position, text, font=font, fill=font_colour, anchor="mm")
+        
+        image1_with_text_array = convert_to_rgb_array(image1_with_text).reshape((-1, 3))
+        image2_with_text_array = convert_to_rgb_array(image2_with_text).reshape((-1, 3))
+        
+        weight_image1 = .5 + (i - 1) * (.5 / (font_colour_index - 1))
+        
+        _images.append(mix(image1_with_text_array, image2_with_text_array, weight_image1))
+        
+        # hacky: save feature image
+        if i == font_colour_index and text == "Wealth":
+            image1_with_text.save("wealth_density_feature_image.png")
+    
+    return create_transition_sequence(_images)
 
+
+def create_transition_sequence(_images: List[np.ndarray]) -> List[np.ndarray]:
+    transition_sequence = []
+    for i in range(len(_images)):
+        if i != 0:    
+            transition_sequence.append(_images[i])
+        if i < len(_images) - 1:
+            transition_sequence.append(mix(_images[i], _images[i+1], .5))
+    return transition_sequence
+    
+    
+population_transition_images = create_transition_images(population_image, wealth_image, "Population")
+wealth_transition_images = create_transition_images(wealth_image, population_image, "Wealth")
+    
+# define image sequence
+FPS = 20
+frames_long_segment = int(FPS * 2.5)
+
+images = []
+images.extend([population_transition_images[-1]] * frames_long_segment)
+images.extend(population_transition_images[::-1])
+images.extend(wealth_transition_images)
+images.extend([wealth_transition_images[-1]] * frames_long_segment)
+images.extend(wealth_transition_images[::-1])
+images.extend(population_transition_images)
+    
 # set up images for animation
 fig = plt.figure(dpi=500, figsize=[5.052151238591917, 2.5], frameon=False) # hacky: adjusted to image size
 fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None) # no white borders
 ax = plt.axes()
 ax.axis("off")
 image = ax.imshow(np.zeros(image_shape))
-fps = 20
-# show each image for 2 seconds and use 1 second for transition between the two
-frames = fps * 6
-frames_long_segment = fps * 2
 
 
 def animate(_i: int) -> AxesImage:
-    """fill image with content for each frame
-    hacky if-else clause to determine which images are used for fade-ins and -outs
-    """
-    if _i <= frames_long_segment:
-        set_image(population_image_with_text_flat_array)
-    elif _i <= round(fps / 2) + frames_long_segment:
-        set_image(fade(population_image_with_text_flat_array, 1 - (2 * (_i - frames_long_segment) / fps)))
-    elif _i <= fps + frames_long_segment:
-        set_image(fade(wealth_image_with_text_flat_array, (2 * (_i - frames_long_segment) / fps) - 1))
-    elif _i <= frames_long_segment * 2 + fps:
-        set_image(wealth_image_with_text_flat_array)
-    elif _i <= round(fps / 2) + frames_long_segment * 2 + fps:
-        set_image(fade(wealth_image_with_text_flat_array, 1 - (2 * (_i - (frames_long_segment * 2 + fps)) / fps)))
-    else:
-        set_image(fade(population_image_with_text_flat_array, (2 * (_i - (frames_long_segment * 2 + fps)) / fps) - 1))
+    """fill image with content for each frame"""
+    set_image(images[_i])
     return [image]
 
 
@@ -186,13 +213,5 @@ def set_image(image_flat_array: np.ndarray) -> np.ndarray:
     image.set_array(image_flat_array.reshape(image_shape))
 
 
-def fade(image_flat_array: np.ndarray, weight_image: float) -> np.ndarray:
-    """mix image with text with a 50/50 mix of population and wealth images without text"""
-    weight_other_images = (1 - weight_image) / 2
-    faded_image = ((image_flat_array * weight_image) + (population_image_flat_array * weight_other_images)
-                   + (wealth_image_flat_array* weight_other_images))
-    return faded_image.astype(int)
-
-
-population_wealth_animation = animation.FuncAnimation(fig, animate, frames=frames)
-population_wealth_animation.save('population_vs_wealth_density.gif', fps=fps)
+population_wealth_animation = animation.FuncAnimation(fig, animate, frames=len(images))
+population_wealth_animation.save('population_vs_wealth_density.gif', fps=FPS)
